ab20a56975eff8f88359fc1c84a7b54a07efb8d4
Jakob Jensen <jakobj@jakobj.dk>
2022-07-10 14:02:33 +0200

    Add quiet mode

--- lane.d/shell-github-action-semver-compare/run.sh
+++ lane.d/shell-github-action-semver-compare/run.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
 
-unset -v MAIN
-unset -v CURRENT
-while getopts "m:c:" option; do
+unset -v main
+unset -v current
+quiet=0
+while getopts "m:c:q" option; do
   case $option in
-  m) MAIN=$OPTARG ;;
-  c) CURRENT=$OPTARG ;;
+  m) main=$OPTARG ;;
+  c) current=$OPTARG ;;
+  q) quiet=1 ;;
   \?) exit 111 ;;
   esac
 done
 shift $((OPTIND - 1))
 
-if [ -z "$MAIN" ] || [ -z "$CURRENT" ]; then
+if [ -z "$main" ] || [ -z "$current" ]; then
   printf 'Provide both main and current versions.\n\n'
   exit 111
 fi
 
-echo '::group::Resolved versions'
-printf '%s - main version\n%s - current version\n' "$MAIN" "$CURRENT"
-echo '::endgroup::'
+if [ $quiet -eq 0 ]; then
+  echo '::group::Resolved versions'
+  printf '%s - main version\n%s - current version\n' "$main" "$current"
+  echo '::endgroup::'
+fi
 
-[ "$MAIN" = "$CURRENT" ] && {
+[ "$main" = "$current" ] && {
   echo 'Version must be changed.'
   exit 10
 }
 
-VERIFY=$(printf '%s\n%s' "$MAIN" "$CURRENT" | sort -t '.' -k 1,1 -k 2,2 -k 3,3 -k 4,4 -k 5,5 -g | tail -n1)
-[ "$MAIN" = "$VERIFY" ] && {
+verify=$(printf '%s\n%s' "$main" "$current" | sort -t '.' -k 1,1 -k 2,2 -k 3,3 -k 4,4 -k 5,5 -g | tail -n1)
+[ "$main" = "$verify" ] && {
   echo 'Version must be greater than version on main.'
   exit 20
 }
