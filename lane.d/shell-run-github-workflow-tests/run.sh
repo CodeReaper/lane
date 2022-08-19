72f0038ab97d8ed716fa8f87a03c578017f8ec41
Jakob Jensen <jakobj@jakobj.dk>
2022-08-19 23:23:25 +0200

    Use python instead of awk for csv parsing

--- lane.d/shell-run-github-workflow-tests/run.sh
+++ lane.d/shell-run-github-workflow-tests/run.sh
@@ -50,7 +50,7 @@ yq -o json "$file" | jq -rc '.jobs | to_entries[] | [{group: .key, groupName: .v
 
   {
     echo "echo; echo '$group_name ($group)'"
-    echo "GREEN=$'\e[0;32m'; RED=$'\e[0;31m'; NC=$'\e[0m'; PASS=0; FAIL=0"
+    echo "GREEN='\e[0;32m'; RED='\e[0;31m'; NC='\e[0m'; PASS=0; FAIL=0"
   } >"${DIR}/${group}.sh"
 
   if [ -n "$jobid" ] && [ ! "$jobid" = "$group" ]; then
