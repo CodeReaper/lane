7e80bd8a8af8d5ed8dc385f4f0069b232cb654e3
Jakob Jensen <jakobj@jakobj.dk>
2023-07-23 13:55:33 +0200

    Sorted static images output (#16)

--- .github/test-resources/mobile-static-resources-images/assets.swift
+++ .github/test-resources/mobile-static-resources-images/assets.swift
@@ -1,5 +1,8 @@
 // swiftlint:disable all
 import UIKit
 struct Images {
+	static let dk1 = UIImage(named:"dk1")!
+	static let dk2 = UIImage(named:"dk2")!
 	static let test = UIImage(named:"test")!
 }
+// swiftlint:enable all
