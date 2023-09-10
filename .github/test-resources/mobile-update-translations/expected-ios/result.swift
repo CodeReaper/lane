a011cc6ca569e66b15156a3ea5d53874996db64b
Jakob Jensen <jakobj@jakobj.dk>
2023-09-10 17:13:33 +0200

    Handle complex xml (#19)

--- .github/test-resources/mobile-update-translations/expected-ios/result.swift
+++ .github/test-resources/mobile-update-translations/expected-ios/result.swift
@@ -3,5 +3,6 @@ import Foundation
 struct Translations {
 	static let SOMETHING = NSLocalizedString("SOMETHING", comment: "")
 	static let SOMETHING_ESCAPED = NSLocalizedString("SOMETHING_ESCAPED", comment: "")
+	static let SOMETHING_FOR_XML = NSLocalizedString("SOMETHING_FOR_XML", comment: "")
 	static func SOMETHING_WITH_ARGUMENTS(_ p1: String, _ p2: String) -> String { return NSLocalizedString("SOMETHING_WITH_ARGUMENTS", comment: "").replacingOccurrences(of: "%1", with: p1).replacingOccurrences(of: "%2", with: p2) }
 }
