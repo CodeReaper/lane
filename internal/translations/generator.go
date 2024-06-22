8d6539c78b79b254883535045451ad4d1f4bfb6f
Jakob Jensen <jakobj@jakobj.dk>
2024-06-23 01:32:27 +0200

    csv is perhaps working

--- internal/translations/generator.go
+++ internal/translations/generator.go
@@ -3,8 +3,15 @@ package translations
 import (
 	"context"
 	"encoding/csv"
-	"log"
+	"encoding/json"
+	"fmt"
+	"io"
 	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 var androidKind = "android"
@@ -21,9 +28,14 @@ type Generator struct {
 	flags *Flags
 }
 
-func NewGenerator(x *Flags) *Generator {
+type Configuration struct {
+	index int
+	path  string
+}
+
+func NewGenerator(f *Flags) *Generator {
 	return &Generator{
-		flags: x,
+		flags: f,
 	}
 }
 
@@ -32,9 +44,33 @@ func (g *Generator) Generate(ctx context.Context) error {
 		return err
 	}
 
-	r, err := os.Open(g.flags.Input)
+	configurations, err := parseConfigurations(g.flags.Configurations)
 	if err != nil {
 		return err
+	}
+
+	records, err := loadRecords(g.flags.Input)
+	if err != nil {
+		return err
+	}
+
+	for _, c := range configurations {
+		if err := writeFile(g.flags.Kind, makeTranslations(records, g.flags.Index-1, c.index-1), c.path); err != nil {
+			return nil
+		}
+	}
+
+	if g.flags.Kind == iosKind {
+		return writeSupportFile(makeTranslations(records, g.flags.Index-1, g.flags.DefaultIndex-1), g.flags.Output)
+	} else {
+		return nil
+	}
+}
+
+func loadRecords(path string) ([][]string, error) {
+	r, err := os.Open(path)
+	if err != nil {
+		return nil, err
 	}
 
 	reader := csv.NewReader(r)
@@ -42,35 +78,208 @@ func (g *Generator) Generate(ctx context.Context) error {
 
 	records, err := reader.ReadAll()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if len(records) > 0 {
 		records = records[1:] // skips header row
 	}
+	return records, nil
+}
 
-	switch g.flags.Kind {
+func makeTranslations(records [][]string, keyIndex int, valueIndex int) map[string]string {
+	translations := map[string]string{}
+	for _, r := range records {
+		translations[strings.ToLower(r[keyIndex])] = r[valueIndex]
+	}
+	return translations
+}
+
+func sortedKeys(array map[string]string) []string {
+	keys := make([]string, 0, len(array))
+	for k := range array {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func parseConfigurations(configurations []string) ([]Configuration, error) {
+	list := make([]Configuration, 0)
+	for _, configuration := range configurations {
+		fields := strings.Fields(configuration)
+		if len(fields) != 2 {
+			return nil, fmt.Errorf("configuration has invalid format: %s", configuration)
+		}
+
+		index, err := strconv.Atoi(fields[0])
+		if err != nil {
+			return nil, fmt.Errorf("configuration has invalid index: %s", configuration)
+		}
+
+		path := fields[1]
+		if _, err := os.Stat(filepath.Dir(path)); err != nil {
+			return nil, fmt.Errorf("configuration has invalid path: %s", configuration)
+		}
+
+		list = append(list, Configuration{
+			index: index,
+			path:  path,
+		})
+	}
+	return list, nil
+}
+
+func writeFile(kind string, translations map[string]string, path string) error {
+	tempPath := path + ".tmp"
+	defer os.Remove(tempPath)
+
+	tempFile, err := os.Create(tempPath)
+	if err != nil {
+		return err
+	}
+	defer tempFile.Close()
+
+	switch kind {
 	case androidKind:
-		return writeAndroid(records, g.flags.Configurations)
+		err = writeAndroid(translations, tempFile)
 	case iosKind:
-		return writeIos(records, g.flags.Configurations)
+		err = writeIos(translations, tempFile)
 	case jsonKind:
-		return writeJson(records, g.flags.Configurations)
+		err = writeJson(translations, tempFile)
 	default:
-		log.Fatalf("found unknown kind: %v", g.flags.Kind)
+		err = fmt.Errorf("found unknown kind: %v", kind)
+	}
+	if err != nil {
+		return err
+	}
+
+	return os.Rename(tempPath, path)
+}
+
+func writeAndroid(translations map[string]string, io io.Writer) error {
+	regex := regexp.MustCompile(`%([0-9]+)`)
+
+	escape := strings.NewReplacer(
+		"&", "&amp;",
+		"<", "&lt;",
+		">", "&gt;",
+		"\"", "\\\"",
+		"'", "\\'",
+		"\n", "\\n")
+
+	_, err := io.Write([]byte("<resources>\n"))
+	if err != nil {
+		return err
+	}
+
+	for _, k := range sortedKeys(translations) {
+		key := strings.ToLower(k)
+		value := regex.ReplaceAllString(translations[k], "%${1}$$s")
+		_, err = io.Write([]byte(fmt.Sprintf("\t<string name=\"%s\">%s</string>\n", key, escape.Replace(value))))
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = io.Write([]byte("</resources>\n"))
+
+	return err
+}
+
+func writeIos(translations map[string]string, io io.Writer) error {
+	escape := strings.NewReplacer(
+		"\"", "\\\"",
+		"\n", "\\n")
+
+	for _, k := range sortedKeys(translations) {
+		key := strings.ToUpper(k)
+		value := translations[k]
+		_, err := io.Write([]byte(fmt.Sprintf("\"%s\" = \"%s\";\n", key, escape.Replace(value))))
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
-func writeAndroid(records [][]string, configurations []string) error {
-	panic("unimplemented")
+func writeJson(translations map[string]string, io io.Writer) error {
+	data := map[string]string{}
+	for k, v := range translations {
+		data[strings.ToLower(k)] = v
+	}
+
+	b, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Write(b)
+
+	return err
 }
 
-func writeIos(records [][]string, configurations []string) error {
-	panic("unimplemented")
+func writeSupportFile(translations map[string]string, path string) error {
+	tempPath := path + ".tmp"
+	defer os.Remove(tempPath)
+
+	tempFile, err := os.Create(tempPath)
+	if err != nil {
+		return err
+	}
+	defer tempFile.Close()
+
+	err = writeSwift(translations, tempFile)
+	if err != nil {
+		return err
+	}
+
+	return os.Rename(tempPath, path)
 }
 
-func writeJson(records [][]string, configurations []string) error {
-	panic("unimplemented")
+func writeSwift(translations map[string]string, io io.Writer) error {
+	regex := regexp.MustCompile(`%([0-9]+)`)
+
+	header := `// swiftlint:disable all
+import Foundation
+struct Translations {
+`
+	footer := `}
+`
+
+	_, err := io.Write([]byte(header))
+	if err != nil {
+		return err
+	}
+
+	for _, k := range sortedKeys(translations) {
+		key := strings.ToUpper(k)
+		value := translations[k]
+
+		var line string
+		matches := regex.FindAllStringSubmatch(value, -1)
+		if len(matches) > 0 {
+			arguments := make([]string, 0)
+			replacements := make([]string, 0)
+			for _, m := range matches {
+				match := m[1]
+				arguments = append(arguments, fmt.Sprintf("_ p%s: String", match))
+				replacements = append(replacements, fmt.Sprintf(".replacingOccurrences(of: \"%%%s\", with: p%s)", match, match))
+			}
+			argumentsString := strings.Join(arguments, ", ")
+			replacementsString := strings.Join(replacements, "")
+			line = fmt.Sprintf("\tstatic func %s(%s) -> String { return NSLocalizedString(\"%s\", comment: \"\")%s }\n", key, argumentsString, key, replacementsString)
+		} else {
+			line = fmt.Sprintf("\tstatic let %s = NSLocalizedString(\"%s\", comment: \"\")\n", key, key)
+		}
+
+		_, err := io.Write([]byte(line))
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = io.Write([]byte(footer))
+	return err
 }
--- internal/translations/generator_test.go
+++ internal/translations/generator_test.go
@@ -8,24 +8,66 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSimple(t *testing.T) {
-	expected := `<resources>
-    <string name="something">Something</string>
-    <string name="something_with_arguments">Something with %1$s and %2$s</string>
-</resources>`
-
+func TestAndroid(t *testing.T) {
 	flags := Flags{
 		Input:          "testdata/input.csv",
 		Kind:           "android",
 		Index:          1,
-		Configurations: []string{"3 build/en.xml"},
+		Configurations: []string{"3 ../../build/en.xml", "4 ../../build/da.xml"},
 	}
 
 	generator := NewGenerator(&flags)
 	err := generator.Generate(context.Background())
 
 	assert.Nil(t, err)
-	b, err := os.ReadFile("build/en.xml")
+	equalFiles(t, "testdata/android-en.expected", "../../build/en.xml")
+	equalFiles(t, "testdata/android-da.expected", "../../build/da.xml")
+}
+
+func TestJson(t *testing.T) {
+	flags := Flags{
+		Input:          "testdata/input.csv",
+		Kind:           "json",
+		Index:          1,
+		Configurations: []string{"3 ../../build/en.json"},
+	}
+
+	generator := NewGenerator(&flags)
+	err := generator.Generate(context.Background())
+
 	assert.Nil(t, err)
-	assert.Equal(t, expected, string(b))
+	equalFiles(t, "testdata/json-en.expected", "../../build/en.json")
+}
+
+func TestIos(t *testing.T) {
+	flags := Flags{
+		Input:          "testdata/input.csv",
+		Kind:           "ios",
+		Index:          1,
+		Configurations: []string{"3 ../../build/en.strings", "4 ../../build/da.strings"},
+		DefaultIndex:   3,
+		Output:         "../../build/Translations.swift",
+	}
+
+	generator := NewGenerator(&flags)
+	err := generator.Generate(context.Background())
+
+	assert.Nil(t, err)
+	equalFiles(t, "testdata/ios-en.expected", "../../build/en.strings")
+	equalFiles(t, "testdata/ios-da.expected", "../../build/da.strings")
+	equalFiles(t, "testdata/ios-swift.expected", "../../build/Translations.swift")
+}
+
+func equalFiles(t *testing.T, expectedPath string, actualPath string) bool {
+	expected, err := os.ReadFile(expectedPath)
+	if !assert.Nil(t, err) {
+		return false
+	}
+
+	actual, err := os.ReadFile(actualPath)
+	if !assert.Nil(t, err) {
+		return false
+	}
+
+	return assert.Equal(t, string(expected), string(actual))
 }
