2b800f221d9f130b0f88290e96711b20deaff3bd
Jakob Jensen <jakobj@jakobj.dk>
2025-04-21 14:20:11 +0200

    Add go report card (#88)

--- internal/translations/flags.go
+++ internal/translations/flags.go
@@ -18,29 +18,50 @@ type Flags struct {
 }
 
 func (f *Flags) validate() error {
+	if err := f.checkRequiredFields(); err != nil {
+		return err
+	}
+
+	isIOS := strings.ToLower(f.Kind) == "ios"
+
+	if err := f.checkRequiredFiles(isIOS); err != nil {
+		return err
+	}
+
+	if isIOS || f.FillIn {
+		if f.DefaultValueIndex <= 0 {
+			return fmt.Errorf("main index not provided")
+		}
+	}
+
+	return nil
+}
+
+func (f *Flags) checkRequiredFields() error {
 	if len(f.Input) == 0 {
 		return fmt.Errorf("input not provided")
-	}
-	if len(f.Kind) == 0 {
-		return fmt.Errorf("kind not provided")
 	}
 	if f.KeyIndex <= 0 {
 		return fmt.Errorf("index not provided")
 	}
-
-	isIOS := false
-
-	validKind := false
-	for _, v := range validKinds {
-		if !validKind && v == strings.ToLower(f.Kind) {
-			validKind = true
-		}
-		isIOS = isIOS || strings.ToLower(f.Kind) == "ios"
+	if len(f.Kind) == 0 {
+		return fmt.Errorf("kind not provided")
 	}
-	if !validKind {
+
+	valid := false
+	for _, v := range validKinds {
+		if v == strings.ToLower(f.Kind) {
+			valid = true
+		}
+	}
+	if !valid {
 		return fmt.Errorf("invalid kind: %s. Valid kinds are %v", f.Kind, validKinds)
 	}
 
+	return nil
+}
+
+func (f *Flags) checkRequiredFiles(isIOS bool) error {
 	if _, err := os.Stat(f.Input); err != nil {
 		return err
 	}
@@ -57,12 +78,6 @@ func (f *Flags) validate() error {
 		}
 		if _, err := os.Stat(filepath.Dir(f.Output)); err != nil {
 			return err
-		}
-	}
-
-	if isIOS || f.FillIn {
-		if f.DefaultValueIndex <= 0 {
-			return fmt.Errorf("main index not provided")
 		}
 	}
 
